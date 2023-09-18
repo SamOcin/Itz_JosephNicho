4ce12e98e24d9d32b539772dd1cee26832316c82
SamPeco <145143146+SamPeco@users.noreply.github.com>
2023-09-18 17:08:39 +0700

    Update script.js

--- about me/script.js
+++ about me/script.js
@@ -1,9 +1,1 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "d") {
-        // Cari elemen <li> pertama dan klik di dalamnya
-        var firstListItem = document.querySelector("#daftar li");
-        if (firstListItem) {
-            firstListItem.click();
-        }
-    }
-});
+<button>mantap</button>
