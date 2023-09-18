2488b6f5ef3d74d091481ea23dacadb0ae87dcf6
SamPeco <145143146+SamPeco@users.noreply.github.com>
2023-09-18 17:24:18 +0700

    Update script.js

--- about me/script.js
+++ about me/script.js
@@ -1,1 +1,10 @@
-<button>mantap</button>
+function updateWaktu() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    
+    document.getElementById('waktu').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateWaktu, 1000);
