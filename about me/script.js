927913708df2cfbc8f019ab8ef7668747f6b70a3
SamPeco <145143146+SamPeco@users.noreply.github.com>
2023-09-18 17:40:21 +0700

    Update script.js

--- about me/script.js
+++ about me/script.js
@@ -4,7 +4,7 @@ function updateWaktu() {
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
     
-    document.getElementById('waktu').textContent = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('waktu').textContent = `WAKTU: ${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(updateWaktu, 1000);
