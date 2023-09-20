57d31656b3db1d041eee69a1a2682857b300fbdd
SamPeco <145143146+SamPeco@users.noreply.github.com>
2023-09-20 19:38:28 +0700

    Delete about me directory

--- about me/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "d") {
-        // Cari elemen <li> pertama dan klik di dalamnya
-        var firstListItem = document.querySelector("#daftar li");
-        if (firstListItem) {
-            firstListItem.click();
-        }
-    }
-});
